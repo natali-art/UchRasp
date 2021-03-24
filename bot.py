30d8bbee7d8664b7f28dc6c5ac173e11d4c41a37
natali-art <81312555+natali-art@users.noreply.github.com>
2021-03-24 18:58:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,16 @@
-import telebot
+import telebothttps://github.com/natali-art/UchRasp/blob/main/bot.py
 bot = telebot.TeleBot('1723704255:AAEMH1m1C5r2WNrKfGkHpLUN_78lu3Yg_xs')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f'Я бот-расписание и помогу тебе не опоздать на урок. Приятно познакомиться, {message.from_user.first_name}')
 
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.from_user.id, 'Привет!')
     else:
-       if message.text.lower() == 'как дела?':
-            bot.send_message(message.from_user.id, 'Отлично!')
-        else:
-            bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 
 bot.polling(none_stop=True)
