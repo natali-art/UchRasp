851f5b1a32e6fa7d4a4ab6bc3d193293d3f0b56b
natali-art <81312555+natali-art@users.noreply.github.com>
2021-03-25 00:45:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
-import telebothttps://github.com/natali-art/UchRasp/blob/main/bot.py
+import telebot
 bot = telebot.TeleBot('1723704255:AAEMH1m1C5r2WNrKfGkHpLUN_78lu3Yg_xs')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f'Я бот-расписание и помогу тебе не опоздать на урок. Приятно познакомиться, {message.from_user.first_name}')
-
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
